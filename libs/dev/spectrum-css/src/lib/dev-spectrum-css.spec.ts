893b5302d5ae021b9b139a09a60859dc0fed272d
Aaron R Miller <amiller@watheia.org>
2022-04-17 08:40:16 -0700

    add spectrum-css assets

--- libs/dev/spectrum-css/src/lib/dev-spectrum-css.spec.ts
+++
@@ -1,7 +0,0 @@
-import { devSpectrumCss } from './dev-spectrum-css';
-
-describe('devSpectrumCss', () => {
-  it('should work', () => {
-    expect(devSpectrumCss()).toEqual('dev-spectrum-css');
-  });
-});
--- libs/dev/spectrum-css/src/lib/dev-spectrum-css.ts
+++
@@ -1,3 +0,0 @@
-export function devSpectrumCss(): string {
-  return 'dev-spectrum-css';
-}
