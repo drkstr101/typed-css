8c3e2906c9258937f940fd6e07aa67f662d8a22e
Aaron R Miller <amiller@watheia.org>
2022-04-18 18:05:49 -0700

    remove uneeded module

--- libs/plugins/rollup/src/lib/plugins-rollup.spec.ts
+++
@@ -1,7 +0,0 @@
-import { pluginsRollup } from './plugins-rollup';
-
-describe('pluginsRollup', () => {
-  it('should work', () => {
-    expect(pluginsRollup()).toEqual('plugins-rollup');
-  });
-});
--- libs/plugins/rollup/src/lib/plugins-rollup.ts
+++
@@ -1,3 +0,0 @@
-export function pluginsRollup(): string {
-  return 'plugins-rollup';
-}
